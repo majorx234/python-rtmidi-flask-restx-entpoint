305391355e14e743296d613335480d1d8f7a07fa
CodingWithLaura <laura.ganss@web.de>
2021-07-23 00:14:25 +0200

    delete wrong __init__function in midi_endpoint ressource

--- resources/midi_endpoint.py
+++ resources/midi_endpoint.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 import rtmidi
 
 status = "ok"
-midi_msg = [0x80, 60, 0]
+#midi_msg = [0x80, 60, 0]
 midiout = rtmidi.MidiOut(rtapi=rtmidi.API_UNIX_JACK)
 rtMidiOutputPorts = midiout.get_ports()
 if rtMidiOutputPorts:
@@ -18,11 +18,6 @@ api = Namespace('midi_endpoint', description='game_ratings related operations')
 
 @api.route('')
 class MidiEndpoint(Resource):
-#    def __init__(self):
-#        self.midi_msg = [0x80, 60, 0]
-#        self.midiout = rtmidi.MidiOut(rtapi=rtmidi.API_UNIX_JACK)    
-#        self.rtMidiOutputPorts = self.midiout.get_ports()
-
 #    def get(self):
 #        ''' send status '''
 #        return status, HTTPStatus.OK
@@ -31,8 +26,8 @@ class MidiEndpoint(Resource):
         '''Adds a game to games'''
         midi_msg_raw = request.json
         print(midi_msg_raw)
-        midi_msg = [int(midi_msg_raw['status']),
-                    int(midi_msg_raw['data1']),
-                    int(midi_msg_raw['data2'])]
+        midi_msg = [int(midi_msg_raw[0]),
+                    int(midi_msg_raw[1]),
+                    int(midi_msg_raw[2])]
         midiout.send_message(midi_msg)
         return status, HTTPStatus.CREATED
